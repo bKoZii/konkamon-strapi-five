ad72976e9694a83caf799cbac5c9ca6119b4c96b
Konkamon Sion <bigkonkamon@gmail.com>
2024-11-27 22:44:49 +0700

    feat: Digital Ocean Prepare

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,15 +1,22 @@
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+   process.env.DATABASE_URL
+ );
+
 export default ({ env }) => ({
-  connection: {
-    client: "postgres",
+  connection: {  
+    client: 'postgres',
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
+      host,
+      port,
+      database,
+      user,
+      password,
       ssl: env.bool("DATABASE_SSL", false) && {
-        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", false),
+        ca: env("DATABASE_SSL_CA", undefined),
       },
     },
+    debug: false,
   },
 });
