46b2f67291aa40c238dce514389aa31bef6e3569
Konkamon Sion <bigkonkamon@gmail.com>
2024-11-28 01:22:12 +0700

    feat(server): change 'host:' to 'url:'

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-    host: env('APP_URL', '0.0.0.0'),
+    url: env('APP_URL', '0.0.0.0'),
     port: env.int('PORT', 1337),
     app: { 
       keys: env.array('APP_KEYS')
